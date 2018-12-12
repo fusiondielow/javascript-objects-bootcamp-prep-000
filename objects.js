0ad9545d1de9acaf983cb9d460bb7e75c17aa738
Nelson Dominguez <nlsn_dominguez@yahoo.com>
2018-12-12 22:15:24 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { beatles: "Hard Day's Nigjt"};
+
+function updatePlaylist(playlist, artist, title){
+  Object.assign(playlist, {[ artist]: "name"})
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  // delete playlist.artist;
+  playlist.delete.artist
+  return playlist
+}
